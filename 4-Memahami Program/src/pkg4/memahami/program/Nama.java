5fcfa69766ebd21c19d870a23d7e693320efcb7e
dna83 <uwatere919@gmail.com>
2019-12-16 19:26:16 +0700

    prak 13-14

---
+++ 4-Memahami Program/src/pkg4/memahami/program/Nama.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.memahami.program;
+
+/**
+ * Nama : Dani Nugraha
+ * NIM  : A2.1900035
+ * Kelas: TI-1C
+ */
+public class Nama {
+    public static void main(String[] args){
+		System.out.print("nama ku adalah Dani, teman si fajar");
+	}
+}
